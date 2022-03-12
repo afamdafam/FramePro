2a62565d8ed6212aea7635d35ecf2416f46787b7
AfamDafam <56785023+afamdafam@users.noreply.github.com>
2022-03-12 22:32:28 +0700

    initial commit

---
+++ PinjemIn/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PinjemIn
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+     
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/style").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/Footer.css",
+                      "~/Content/Style.css"));
+            bundles.Add(new StyleBundle("~/Content/footer").Include(
+                      "~/Content/Footer.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/Footer.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ PinjemIn/Controllers/q1Controller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PinjemIn.Controllers
+{
+    public class q1Controller : Controller
+    {
+        // GET: q1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            return View();
+        }
+        public ActionResult Company()
+        {
+            return View();
+        }
+        public ActionResult Service()
+        {
+            return View();
+        }
+        public ActionResult Collaboration()
+        {
+            return View();
+        }
+    }
+}
